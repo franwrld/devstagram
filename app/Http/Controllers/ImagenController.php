dcc3a5e92ca0ce69ae700c1731613689d0160eff
franwrld <franwrld22@gmail.com>
2025-03-20 13:40:27 -0600

    Configuracion guardar en uploads y fit 1000x1000 image agregada

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
-        return "desde imagen controller";
+        $imagen = $request->file('file');
+        // uuid permite crear un id unico a cada imagen
+        $nombreImagen = Str::uuid() . '.' . $imagen->extension();
+        // Instaciamos la imagen a procesar
+        $imagenServidor =  Image::make($imagen);
+        // Funcion que permite darle cortar la imagen al tamano deseado
+        $imagenServidor->fit(1000, 1000);
+        //Guardamos la imagen en uploads $nombreImagen guarda el nombre unico de la imagen en carpeta uploads ejemplo uploads/124148478278.jpg
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+        // Guardamos la imagen subida con la ruta asignada y formato
+        $imagenServidor->save($imagenPath);
+
+        return response()->json(['imagen' => $nombreImagen ]);
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,26 @@ import Dropzone from "dropzone";
 
 Dropzone.autoDiscover = false;
 
-document.addEventListener('DOMContentLoaded', () => {
-    const dropzone = new Dropzone('#dropzone', {
-        dictDefaultMessage: 'Sube aqui tu imagen',
-        acceptedFiles: ".png, .jpg, .jpeg, .gif",
-        addRemoveLinks: true,
-        dictRemoveFile: 'Borrar Archivo',
-        maxFiles: 1,
-        uploadMultiple: false
-    });
+
+const dropzone = new Dropzone('#dropzone', {
+    dictDefaultMessage: 'Sube aqui tu imagen',
+    acceptedFiles: ".png, .jpg, .jpeg, .gif",
+    addRemoveLinks: true,
+    dictRemoveFile: 'Borrar Archivo',
+    maxFiles: 1,
+    uploadMultiple: false
+});
+//Debug
+dropzone.on('sending', function(file, xhr, formData) {
+    console.log(file);
+});
+
+dropzone.on('success', function(file, response) {
+    console.log(response);
+});
+dropzone.on('error', function(file, message) {
+    console.log(message);
+});
+dropzone.on('removedFile', function() {
+    console.log("Archivo Eliminado");
 });
