52cb3ddcc8b8461569796aa6d2f9b87eaacfd93d
franwrld <franwrld22@gmail.com>
2025-03-19 18:52:02 -0600

    Dashborad desing info added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,31 @@
 @extends('layouts.app')
 
 @section('titulo')
-    Tu Cuenta
+    Perfil: {{ $user->username }}
 @endsection
 
 @section('contenido')
     <div class="flex justify-center">
-        <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
-            <div class="md:w-8/12 lg:w-6/12 px-5">
+        <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
+            <!-- Foto de Perfil -->
+            <div class="w-8/12 lg:w-6/12 px-5">
                 <img src="{{ asset('img/usuario.svg')}}" alt="imagen-usuario">
             </div>
-            <div class="md:w-8/12 lg:w-6/12 px-5">
-                <p class="text-gray-700 text-2xl">{{ auth()->user()->username }}</p>
+            <!-- Informacion Perfil -->
+            <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start md:py-10 py-10">
+                <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
+
+                <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
+                    0 <span class="font-normal"> Seguidores</span>
+                </p>
+
+                <p class="text-gray-800 text-sm mb-3 font-bold ">
+                    0 <span class="font-normal"> Seguiendo</span>
+                </p>
+
+                <p class="text-gray-800 text-sm mb-3 font-bold ">
+                    0 <span class="font-normal"> Publicaciones</span>
+                </p>
             </div>
 
         </div>
