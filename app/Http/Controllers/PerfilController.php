ffda2dcd4eecada7fa4bae4d46182cbf81014a40
franwrld <franwrld22@gmail.com>
2025-03-21 14:23:55 -0600

    Validaciones username en editar perfil hechas

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PerfilController extends Controller
@@ -15,5 +16,22 @@ class PerfilController extends Controller
     public function index()
     {
         return view('perfil.index');
+    }
+
+    public function store(Request $request)
+    {
+        // modificar el reequest
+        $request->merge(['username' => Str::slug($request->username)]);
+        // validaciones pervenir que tenga otro usuario mismo username con unique users, si son mas de 3 validacines van en arrays
+        // not_in usernamaes prohibidos y 'unique:users,username,' . auth()->user()->id, para verificar si es el mismo username existente
+        $request->validate([
+            'username' => [
+            'required',
+            'unique:users,username,' . auth()->user()->id,
+            'min:3',
+            'max:25', 
+            'not_in:editar-perfil,login,logout,crear-cuenta,dashboard,nazi'
+            ]
+        ]);
     }
 }
